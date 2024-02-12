137371fcaae908d3ee8ee32ce280a53dab389ba7
Haileab <womahaileab1428@gmail.com>
2024-02-12 18:14:06 +0300

    added some routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,25 @@ import {createTheme} from "@mui/material";
 import { useMemo } from "react";
 import {useSelector } from "react-redux";
 import { themeSettings } from "./theme";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
+import Dashboard from "./scenes/Dashboard/Dashboard";
+import Layout from "./scenes/Layout";
 const App= ()=> {
   const  {mode} = useSelector((state)=> state.global)
   const theme = useMemo(()=> createTheme(themeSettings(mode), [mode]))
   return (
     <div className="app">
-        <ThemeProvider theme={theme}>
-      < CssBaseline/>
-    </ThemeProvider>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+              < CssBaseline/>
+              <Routes>
+                <Route element={<Layout/>}/>
+                <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+              </Routes>
+        </ThemeProvider>
+        </BrowserRouter> 
+       
     </div>
     
   );
